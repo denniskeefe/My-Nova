0178f786a0a7e1289eae8bad90f0a0cdf008233a
Dennis Keefe <dennis@thecommongeek.com>
2013-08-07 00:10:05 -0500

    initial

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
+<<<<<<< HEAD
+# ReadMe
+
+
+## Deployment
+
+```
+ey deploy
+```
+Remove
+## Thanks
+
+The original scaffold for this application was created by [App Scrolls](http://appscrolls.org).
+
+The project was created with the following scrolls:
+
+* rails_basics
+* twitter_bootstrap
+* simple_form
+
+=======
 My-Nova
 =======
+>>>>>>> a01cb98ae81c9595e9b345dfad65268c5bba77b5
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nova::Application.config.secret_token = '3ba48cb1d17cf1cb41c219bffc90b39296d8675fead880a5f0d07eeb30f34e4b953333d4abb553d28653fbdaeb917f32714b187f47bf93fbbc2be4d893bd2da9'
